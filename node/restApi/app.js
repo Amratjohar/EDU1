6d23f710d24ded34788ab7104bf53a0b4c60b53a
Amratjohar <amratjohar@gmail.com>
2023-07-07 17:20:46 +0530

    added login

--- node/restApi/app.js
+++ node/restApi/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const mongo = require("mongodb");
-const mongoClient = mongo.MongoClient;
-const PORT = 5000;
+const MongoClient = mongo.MongoClient;
+const bodyParser = require("body-parser");
 const app = express();
+const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+dotenv.config();
+var cors = require("cors");
+app.use(cors());
+let db;
 
-const MONGO_URL = "mongodb://127.0.0.1:27017";
+//middleware
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//console.log(process.env);
+const MONGO_URL = process.env.MONGO_URL;
 
 //REST API endpoints
 app.get("/", function (req, res) {
@@ -12,6 +23,7 @@ app.get("/", function (req, res) {
 });
 
 //get locations
+
 app.get("/locations", function (req, res) {
   db.collection("location")
     .find()
@@ -20,6 +32,8 @@ app.get("/locations", function (req, res) {
       res.send(result);
     });
 });
+
+//get mealType
 
 app.get("/quickSearch", function (req, res) {
   db.collection("mealType")
@@ -114,12 +128,82 @@ app.get("/menu/:id", function (req, res) {
     });
 });
 
+//place order
+
+app.post("/placeorder", function (req, res) {
+  console.log(req.body);
+  db.collection("orders").insert(req.body, (err, result) => {
+    if (err) throw err;
+    res.send("Order Placed");
+  });
+});
+
+//Menu Details
+
+app.post("/menuItem", function (req, res) {
+  if (Array.isArray(req.body.id)) {
+    db.collection("menu")
+      .find({ menu_id: { $in: req.body.id } })
+      .toArray((err, result) => {
+        if (err) throw err;
+        res.send(result);
+      });
+  } else {
+    res.send("Invalid input");
+  }
+});
+
+//list of orders
+
+app.get("/orders", function (req, res) {
+  let query = {};
+  let email = req.query.email;
+  if (email) {
+    query = { email };
+  }
+  db.collection("orders")
+    .find(query)
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+});
+
+//update Order
+
+app.put("/updateOrder/:id", function (req, res) {
+  let oid = Number(req.params.id);
+  db.collection("orders").updateOne(
+    { orderId: oid },
+    {
+      $set: {
+        status: req.body.status,
+        bank_name: req.body.bank_name,
+        date: req.body.date,
+      },
+    },
+    (err, result) => {
+      if (err) throw err;
+      res.send("Order updated");
+    }
+  );
+});
+
+//delete orders
+
+app.delete("/deleteOrder/:id", function (req, res) {
+  let _id = mongo.ObjectId(req.params.id);
+  db.collection("orders").deleteOne({ _id }, (err, result) => {
+    if (err) throw err;
+    res.send("Order Deleted");
+  });
+});
+
 //Mongodb connection
-mongoClient.connect(MONGO_URL, (err, client) => {
+MongoClient.connect(MONGO_URL, (err, client) => {
   console.log("Mongo is connected");
   if (err) console.log("Error while connecting");
-  db = client.db("Edu2023");
-
+  db = client.db("Edu20239");
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
